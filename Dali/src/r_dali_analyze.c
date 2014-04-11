6fb14c66be28774ea237cb330aa62a89695fa722
Ajay <ajay@moxiedevices.com>
2014-04-11 21:51:15 +0530

    restructured file after g=GG command
    Signed-off-by: Ajay <ajay@moxiedevices.com>

--- Dali/src/r_dali_analyze.c
+++ Dali/src/r_dali_analyze.c
@@ -1,38 +1,38 @@
 /*******************************************************************************
-* DISCLAIMER
-* This software is supplied by Renesas Electronics Corporation and is only
-* intended for use with Renesas products. No other uses are authorized. This
-* software is owned by Renesas Electronics Corporation and is protected under
-* all applicable laws, including copyright laws.
-* THIS SOFTWARE IS PROVIDED "AS IS" AND RENESAS MAKES NO WARRANTIES REGARDING
-* THIS SOFTWARE, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING BUT NOT
-* LIMITED TO WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
-* AND NON-INFRINGEMENT. ALL SUCH WARRANTIES ARE EXPRESSLY DISCLAIMED.
-* TO THE MAXIMUM EXTENT PERMITTED NOT PROHIBITED BY LAW, NEITHER RENESAS
-* ELECTRONICS CORPORATION NOR ANY OF ITS AFFILIATED COMPANIES SHALL BE LIABLE
-* FOR ANY DIRECT, INDIRECT, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES FOR
-* ANY REASON RELATED TO THIS SOFTWARE, EVEN IF RENESAS OR ITS AFFILIATES HAVE
-* BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
-* Renesas reserves the right, without notice, to make changes to this software
-* and to discontinue the availability of this software. By using this software,
-* you agree to the additional terms and conditions found by accessing the
-* following link:
-* http://www.renesas.com/disclaimer
-*
-* Copyright (C) 2011 Renesas Electronics Corporation. All rights reserved.
-*******************************************************************************/
+ * DISCLAIMER
+ * This software is supplied by Renesas Electronics Corporation and is only
+ * intended for use with Renesas products. No other uses are authorized. This
+ * software is owned by Renesas Electronics Corporation and is protected under
+ * all applicable laws, including copyright laws.
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND RENESAS MAKES NO WARRANTIES REGARDING
+ * THIS SOFTWARE, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING BUT NOT
+ * LIMITED TO WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NON-INFRINGEMENT. ALL SUCH WARRANTIES ARE EXPRESSLY DISCLAIMED.
+ * TO THE MAXIMUM EXTENT PERMITTED NOT PROHIBITED BY LAW, NEITHER RENESAS
+ * ELECTRONICS CORPORATION NOR ANY OF ITS AFFILIATED COMPANIES SHALL BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES FOR
+ * ANY REASON RELATED TO THIS SOFTWARE, EVEN IF RENESAS OR ITS AFFILIATES HAVE
+ * BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ * Renesas reserves the right, without notice, to make changes to this software
+ * and to discontinue the availability of this software. By using this software,
+ * you agree to the additional terms and conditions found by accessing the
+ * following link:
+ * http://www.renesas.com/disclaimer
+ *
+ * Copyright (C) 2011 Renesas Electronics Corporation. All rights reserved.
+ *******************************************************************************/
 
 /*******************************************************************************
-* File Name : DALI_analyze.c
-* Device(s) : 
-* Description : This file implement DALI protocol. 
-******************************************************************************/
+ * File Name : DALI_analyze.c
+ * Device(s) : 
+ * Description : This file implement DALI protocol. 
+ ******************************************************************************/
 
 #pragma	SFR
 
 /******************************************************************************
-Includes <System Includes> , ÅgProject IncludesÅh
-******************************************************************************/
+  Includes <System Includes> , ÅgProject IncludesÅh
+ ******************************************************************************/
 #include <stdlib.h>
 #include <string.h>
 #include "r_macrodriver.h"
@@ -43,24 +43,24 @@ Includes <System Includes> , ÅgProject IncludesÅh
 #include "r_dali_timer.h"
 
 /******************************************************************************
-Private global variables and functions
-******************************************************************************/
+  Private global variables and functions
+ ******************************************************************************/
 static uint8_t DALI_Check2ndCommand( uint8_t ucCommandByte );
 static uint8_t DALI_CheckAddress(uint8_t ucAddressByte);
 
 
 /******************************************************************************
-* Function Name : DALI_CheckConfigCommand
-* Description : Check the two commannds or not for configuration command.
-* Argument : command
-* Return Value : TRUE:continue, FALSE:skip
-******************************************************************************/
+ * Function Name : DALI_CheckConfigCommand
+ * Description : Check the two commannds or not for configuration command.
+ * Argument : command
+ * Return Value : TRUE:continue, FALSE:skip
+ ******************************************************************************/
 uint8_t DALI_CheckConfigCommand( uint8_t command )
 {
 	uint8_t ret = FALSE;
-	
+
 	/* Configuration commands(32...129) must be sent 2x within 100ms. */
-	
+
 	/* If 1st command received, set the timer count and countdown. */
 	/* If 2nd command received within 100ms is not same as the 1st, the commands are ignored. */  
 	if (DALI_IsTimerRunning( TIMER_CMD )){
@@ -80,15 +80,15 @@ uint8_t DALI_CheckConfigCommand( uint8_t command )
 }
 
 /******************************************************************************
-* Function Name : DALI_Check2ndCommand
-* Description : Check the two commannds or not.
-* Argument : command
-* Return Value : TRUE:continue, FALSE:skip
-******************************************************************************/
+ * Function Name : DALI_Check2ndCommand
+ * Description : Check the two commannds or not.
+ * Argument : command
+ * Return Value : TRUE:continue, FALSE:skip
+ ******************************************************************************/
 uint8_t DALI_Check2ndCommand( uint8_t command )
 {
 	/* Configuration commands(32...129) must be sent 2x within 100ms. */
-	
+
 	/* If the timer countdown running, check this command same as the last command or not. */
 	if ( DALI_IsTimerRunning( TIMER_CMD )){
 		if (dali_last_command[dali_led_number] != command ){
@@ -102,11 +102,11 @@ uint8_t DALI_Check2ndCommand( uint8_t command )
 }
 
 /******************************************************************************
-* Function Name : DALI_AnalyzeCommand
-* Description : Analyze the DALI command.
-* Argument : dalicommand
-* Return Value : none
-******************************************************************************/
+ * Function Name : DALI_AnalyzeCommand
+ * Description : Analyze the DALI command.
+ * Argument : dalicommand
+ * Return Value : none
+ ******************************************************************************/
 void DALI_AnalyzeCommand(uint16_t dalicommand)
 {
 	uint8_t cmd_address = (uint8_t)((dalicommand >> 8) & 0x00FF);
@@ -122,11 +122,11 @@ void DALI_AnalyzeCommand(uint16_t dalicommand)
 				}else{
 					DALI_StopTimer( TIMER_DAPC_INTERVAL );
 				}
-				
+
 				if ( DALI_Check2ndCommand( cmd_data ) ){
 					DALI_SetArcPowerWithFade(cmd_data);
 				}
-				
+
 			}else{
 				/* Command */
 				DALI_StopTimer( TIMER_DAPC_INTERVAL );
@@ -143,20 +143,20 @@ void DALI_AnalyzeCommand(uint16_t dalicommand)
 						}
 					}else if (cmd_data<COMMAND_QUERY_STATUS){
 						/* reserved */
-					
+
 					}else if (cmd_data<0xE0){
 						/* Query Commands */ 	
 						DALI_QueryCommand( cmd_data );
-						
+
 					}else{
 						/* Application Extended Commands */ 	
 						//ä÷êîÇópà”Ç∑ÇÈÅB
-					
+
 					}
 				}
 			}
 		}
-		
+
 	}else{
 		/* Special Command */
 		if ( DALI_Check2ndCommand( cmd_address ) ){ 
@@ -167,11 +167,11 @@ void DALI_AnalyzeCommand(uint16_t dalicommand)
 }
 
 /******************************************************************************
-* Function Name : DALI_CheckAddress
-* Description : Check the address byte.
-* Argument : cmd_address
-* Return Value : TRUE:continue, FALSE:skip
-******************************************************************************/
+ * Function Name : DALI_CheckAddress
+ * Description : Check the address byte.
+ * Argument : cmd_address
+ * Return Value : TRUE:continue, FALSE:skip
+ ******************************************************************************/
 uint8_t DALI_CheckAddress(uint8_t  cmd_address)
 {
 	uint8_t ret = FALSE;
@@ -188,7 +188,7 @@ uint8_t DALI_CheckAddress(uint8_t  cmd_address)
 		/* group address */
 		uint8_t group_address = (cmd_address>>1)&0x0f;
 		uint8_t mask_group = MASK_GROUP[group_address];
-	
+
 		if ( group_address < 8 ){
 			if ((dali_current_variable->group_0_7 & mask_group)){
 				ret = TRUE;
@@ -198,7 +198,7 @@ uint8_t DALI_CheckAddress(uint8_t  cmd_address)
 				ret = TRUE;
 			}
 		}
-		
+
 	}
 	return ret;
 }
